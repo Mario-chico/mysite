90cee3b3e50f293212aa99887f8bd8965bd64308
Mario Chico <chico.developer@hotmail.com>
2024-03-29 21:12:11 -0600

    Dark mode

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,4 +1,17 @@
-document.querySelector(".hamburger")?.addEventListener("click", () => {
-  document.querySelector(".nav-links")?.classList.toggle("expanded");
-  console.log("click compa");
-});
+// document.querySelector(".hamburger")?.addEventListener("click", () => {
+//   document.querySelector(".nav-links")?.classList.toggle("expanded");
+//   console.log("click compa");
+// });
+
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+
+function toggleHamburger() {
+  navMenu?.classList.toggle("show");
+  hamburgerBtn?.setAttribute(
+      "aria-expanded",
+      hamburgerBtn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+  );
+}
+
+hamburgerBtn?.addEventListener("click", toggleHamburger)
