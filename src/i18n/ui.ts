88f5e7fafbcaf4adda4be7fe331d1f216891df6a
Mario Chico <chico.developer@hotmail.com>
2024-04-30 09:29:26 -0600

    Haciendo el blog en español, con rutas en los dos idiomas (es, en) pagina de blog con una lista de los blogs. Traduccion de ui. Agregando utilidades a i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  es: 'Spanish',
+  en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Casa',
+    'nav.about': 'Acerca de',
+    'nav.twitter': 'Twitter',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+} as const;
+export const showDefaultLang = false;
