aa08269ee9de0786dcf29ddd5a4fafc0246f93c9
Mario Chico <chico.developer@hotmail.com>
2024-06-03 08:37:15 -0600

    Arreglando la distancia entre la etiqueta a y el contenedor del blog. Quitando color de la etiqueta a para volver a los blogs, y haciendo que su traduccion aparezca

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,12 +9,14 @@ export const ui = {
   es: {
     'nav.home': 'Inicio',
     'nav.about': 'Acerca de',
-    'about.paragraph': "Soy la reata"
+    'about.paragraph': "Soy la reata",
+    'blog.back': 'Volver',
   },
   en: {
     'nav.home': 'Home',
     'nav.about': 'About',
-    'about.paragraph': "I'm the cock"
+    'about.paragraph': "I'm the cock",
+    'blog.back': 'Back',
   },
 } as const;
 export const showDefaultLang = false;
