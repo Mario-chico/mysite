2aefc24df7e8da42f255c8961268d5181ab3b78d
Mario Chico <chico.developer@hotmail.com>
2024-06-12 15:22:27 -0600

    definiendo la tabla de la bd

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,15 @@
-import { defineDb } from 'astro:db';
+import { column, count, defineDb, defineTable } from 'astro:db';
+
+const Views = defineTable({
+  columns: {
+    slug: column.text({primaryKey: true}),
+    count: column.number({
+      default: 1
+    }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {Views},
 });
