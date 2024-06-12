7075ee2556d588f3779c4e231fcc58438b3db89f
Mario Chico <chico.developer@hotmail.com>
2024-06-12 14:56:48 -0600

    Agregando db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,8 +12,8 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  integrations: [mdx()],
+  integrations: [mdx(), db()],
   image: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com"]
   }
 });
