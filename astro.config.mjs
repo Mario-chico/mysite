2c26af874a5fb8542db15b822bd2744b3559dce4
Mario Chico <chico.developer@hotmail.com>
2024-07-08 09:04:43 -0600

    Liberando con astro studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import db from "@astrojs/db";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
