0a1d0224a4ce1ef4307c9fad20bbef1aeff2645b
Mario Chico <chico.developer@hotmail.com>
2025-02-07 08:04:17 -0600

    Actualizando archivo de configuracion de svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
 import { vitePreprocess } from '@astrojs/svelte';
 
+// svelte.config.js
 export default {
+  // svelte options
+  extensions: ['.svelte'],
+  compilerOptions: {},
 	preprocess: vitePreprocess(),
-}
+  onwarn: (warning, handler) => handler(warning),
+  // plugin options
+  vitePlugin: {
+    exclude: [],
+    // experimental options
+    experimental: {}
+  }
+};
